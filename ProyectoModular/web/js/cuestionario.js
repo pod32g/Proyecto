8522d8c5c758665f380cfe76cfbcbde7ddd1f336
Paul <paul.hermosillo.haro@gmail.com>
2018-08-12 19:37:05 -0500

    Front end y unas mejoras

---
+++ ProyectoModular/web/js/cuestionario.js
@@ -0,0 +1,31 @@
+var cuestionarioViewModel = new CuestionarioViewModel();
+$(document).ready(function(){
+	$.getJSON("http://localhost:4567/Cuestionario", function(cuestionarioJSON){
+		cuestionarioViewModel.preguntas(cuestionarioJSON.visual);
+		ko.utils.arrayPushAll(cuestionarioViewModel.preguntas, cuestionarioJSON.auditivo);
+		ko.utils.arrayPushAll(cuestionarioViewModel.preguntas, cuestionarioJSON.kinestesico);
+
+	});
+	ko.applyBindings(cuestionarioViewModel);
+});
+
+function sendAnswers(){
+		var respuestasJSON = {
+			visual : [],
+			auditivo : [],
+			kinestesico : []
+		};
+		var respuestas = $("input:checked");
+		for (var i = 0; i < respuestas.length; i++) {
+			if(i < 8){
+				respuestasJSON.visual.push(respuestas[i].value);
+			}else if(i >= 8 && i < 15){
+				respuestasJSON.auditivo.push(respuestas[i].value);
+			}else if(i >= 15){
+				respuestasJSON.kinestesico.push(respuestas[i].value);
+			}
+		}
+		$.post("http://localhost:4567/Cuestionario/Procesar", JSON.stringify(respuestasJSON), function(data){
+			cuestionarioViewModel.tipo(data);
+		});
+	}
